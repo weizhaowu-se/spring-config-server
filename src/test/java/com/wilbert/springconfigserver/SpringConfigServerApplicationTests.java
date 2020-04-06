b64ec518983005877c884cef97e93c9678ac6a0f
weizhaowu@outlook.com <weizhaowu@outlook.com>
2020-04-06 15:21:50 +0800

    init

---
+++ src/test/java/com/wilbert/springconfigserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilbert.springconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
